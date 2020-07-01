08afba5df8ececf0a8f32a642b71273186a8b25d
unknown <stdj.1907@gmail.com>
2020-07-01 14:35:34 +0200

    btn-hold added function

--- app.js
+++ app.js
@@ -36,20 +36,49 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     if (dice !== 1) {
         roundScore += dice;
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        
         //add score
     } else {
         //next player roll
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        roundScore = 0;
-
-        document.getElementById('current-1').textContent = '0';
-        document.getElementById('current-0').textContent = '0';
-
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-
-
+        nextPlayer ();
     }
 
 
-} )
+} );
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+  //add current score to global score
+    scores[activePlayer] += roundScore;
+
+
+  //update UI 
+
+  document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+  // if player won the game
+    if (scores[activePlayer] >= 10) {
+        document.querySelector('#name-' + activePlayer).textContent = 'WINNER!!!'
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-'+ activePlayer +'-panel').classList.add('winner');
+        document.querySelector('.player-'+ activePlayer +'-panel').classList.remove('active');
+    } else {
+        nextPlayer();
+    }
+  //next player
+ 
+
+
+});
+
+function nextPlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none';
+}
