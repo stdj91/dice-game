e3c06e7b7146685fcd567c863311ce3404c5fce6
unknown <stdj.1907@gmail.com>
2020-07-01 15:02:52 +0200

    btn-new game done. Add finished.

--- app.js
+++ app.js
@@ -11,16 +11,7 @@ GAME RULES:
 
 var scores, roundScore, activePlayer;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-
-document.querySelector('.dice').style.display = 'none';
+init();
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
     
@@ -36,23 +27,19 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     if (dice !== 1) {
         roundScore += dice;
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
-        
+
         //add score
     } else {
         //next player roll
         nextPlayer ();
     }
-
-
 } );
 
 document.querySelector('.btn-hold').addEventListener('click', function(){
   //add current score to global score
     scores[activePlayer] += roundScore;
 
-
   //update UI 
-
   document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
   // if player won the game
@@ -64,10 +51,6 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
     } else {
         nextPlayer();
     }
-  //next player
- 
-
-
 });
 
 function nextPlayer() {
@@ -82,3 +65,20 @@ function nextPlayer() {
 
     document.querySelector('.dice').style.display = 'none';
 }
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init() {
+    scores = [0,0];
+    roundScore = 0;
+    activePlayer = 0;
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+
+    document.querySelector('.dice').style.display = 'none';
+};
